c5ff2e1c9728a7f184232a68bb06536d8b6a7131
Tune2k <tune2k4@gmail.com>
2025-03-29 13:47:49 +0700

    update ASP.NET

---
+++ TranNhatTu_2122110250/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TranNhatTu_2122110250.Model;
+
+namespace TranNhatTu_2122110250.Data
+{
+	public class AppDbContext : DbContext
+	{
+		public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+		public DbSet<Product> Products { get; set; }
+	}
+}
---
+++ TranNhatTu_2122110250/Model/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace TranNhatTu_2122110250.Model
+{
+	public class Product
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string Image { get; set; }
+		public Double Price { get; set; }
+	}
+}
